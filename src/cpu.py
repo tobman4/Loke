44149d6950fba0a4068f328e8a9d5d8436b0f151
Tobias Lisbakken <tl@braum.tobman.no>
2026-01-13 14:27:14 +0100

    add uptime

--- src/cpu.py
+++ src/cpu.py
@@ -8,6 +8,18 @@ USAGE_GAUGE = Gauge(
   labelnames=["host"]
 )
 
+UPTIME_GAUGE = Gauge(
+    "host_uptime",
+    "Host uptime in seconds",
+    labelnames=["host"]
+)
+
 def export(args: Namespace):
-  usage = psutil.cpu_percent(interval=1)
-  USAGE_GAUGE.labels(args.hostname).set(usage)
+    usage = psutil.cpu_percent(interval=1)
+    USAGE_GAUGE.labels(args.hostname).set(usage)
+
+    boot_time_timestamp = psutil.boot_time()
+    current_time_timestamp = time.time()
+    uptime_seconds = current_time_timestamp - boot_time_timestamp
+    UPTIME_GAUGE.labels(args.hostname).set(uptime_seconds)
+
